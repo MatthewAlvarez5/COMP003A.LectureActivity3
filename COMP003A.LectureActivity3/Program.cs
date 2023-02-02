3df553317627a39cceeb3f9d80243ae430ce53a3
MatthewAlvarez5 <alvarezmatthew2002@gmail.com>
2023-02-01 17:07:15 -0800

    2/1/22

--- COMP003A.LectureActivity3/Program.cs
+++ COMP003A.LectureActivity3/Program.cs
@@ -1,4 +1,9 @@
-﻿namespace COMP003A.LectureActivity3
+﻿/*
+ * Author: Matthew Alvarez
+ * Course: COMP003A
+ * Purpose: Week 3 Lecture
+ */
+namespace COMP003A.LectureActivity3
 {
     internal class Program
     {
